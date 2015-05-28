acb0990556c7644825110b5e73e682e2a7ea55f9
Richard Shade <rshade@rightscale.com>
2015-05-28 11:14:09 -0500

    adding :: to file

--- providers/agent_java.rb
+++ providers/agent_java.rb
@@ -81,7 +81,7 @@ end
 
 def allow_app_group_write_to_log_file_path
   path = new_resource.logfile_path
-  until path.nil? || path.empty? || path == File::SEPARATOR
+  until path.nil? || path.empty? || path == ::File::SEPARATOR
     directory path do
       group new_resource.app_group
       mode 0775
